fd397439d9e522ec08f1d923dfe6735c9fc4139f
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2024-01-27 10:10:41 +0200

    Update operarions.cpp

--- vector/operarions.cpp
+++ vector/operarions.cpp
@@ -105,6 +105,49 @@ public:
         ++size;
 
     }
+    void rigth_rotation()
+    {
+        int last_element = arr[size - 1];
+        for (int p = size - 2; p >= 0; p--)
+            arr[p + 1] = arr[p];
+        arr[0] = last_element;
+    }
+    void left_rotation()
+    {
+        int first_element = arr[0];
+        for (int p = 1; p < size; p++)
+            arr[p - 1] = arr[p];
+        arr[size - 1] = first_element;
+    }
+    void right_rotate(int times) 
+    {
+        times %= size;
+        while (times--)
+            right_rotate(times);
+    }
+
+    int pop(int idx) 
+    {
+        assert(0 <= idx && idx < size);
+        int val = arr[idx];
+        for (int p = idx + 1; p < size; ++p)
+            arr[p - 1] = arr[p];
+
+        --size;
+        return val;
+    }
+
+    int find_transposition(int value) 
+    {
+        for (int i = 0; i < size; ++i)
+            if (arr[i] == value) 
+            {
+                if (i == 0)	
+                    return 0;
+                swap(arr[i], arr[i - 1]);
+                return i - 1;	
+            }
+        return -1;	
 };
 
 int main()
